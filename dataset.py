Dropout, group normalization, batch loss simulation fix, image transformations as two-stream

dataset.py
@@ -4,18 +4,42 @@ from torch.utils.data import Dataset
 import os, pickle
 from torchvision import transforms
 import numpy as np
+import video_transforms
+import cv2
+
+import sys
 
 
 class UCF101(Dataset):
     def __init__(self, zp_limit, ze_limit, root, split_file_path, n_frames=25, transform = None):
         self.transform = transform
         if transform is None:
-            self.transform = transforms.Compose([
-                transforms.Resize(256),
-                transforms.CenterCrop(224),
-                transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-            ])
+            # self.transform = transforms.Compose([
+            #     transforms.Resize(256),
+            #     transforms.CenterCrop(224),
+            #     transforms.ToTensor(),
+            #     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            # ])
+            is_color = True
+            scale_ratios = [1.0, 0.875, 0.75, 0.66]
+            clip_mean = [0.485, 0.456, 0.406]
+            clip_std = [0.229, 0.224, 0.225]
+            normalize = video_transforms.Normalize(mean=clip_mean, std=clip_std)
+            if 'train' in split_file_path:
+                self.transform = video_transforms.Compose([
+                    # video_transforms.Scale((256)),
+                    video_transforms.MultiScaleCrop((224, 224), scale_ratios),
+                    video_transforms.RandomHorizontalFlip(),
+                    video_transforms.ToTensor(),
+                    normalize,
+                ])
+            else:
+                self.transform = video_transforms.Compose([
+                    # video_transforms.Scale((256)),
+                    video_transforms.CenterCrop((224)),
+                    video_transforms.ToTensor(),
+                    normalize,
+                ])
         self.zp_limit = zp_limit
         self.ze_limit = ze_limit
         self.root = root
@@ -23,23 +47,40 @@ class UCF101(Dataset):
         for line in open(split_file_path):
             file_name, duration_str, target_str = line.split(' ')
             duration, target = int(duration_str), int(target_str)
+            # TEMP
+            if 'HandStand' in file_name:
+                file_name = file_name.replace('HandStand', 'Handstand')
             frames_path = os.path.join(self.root, file_name)
-            diff = duration / (n_frames - 1)
+            diff = (duration - 1) / (n_frames - 1)
             frames_ids = (np.arange(n_frames) * diff + 1).astype(np.int)
             new_data = [frames_path, frames_ids, target]
             data.append(new_data)
         self.data = np.array(data)
     
     def get_transformed_frame(self, path):
-        img = Image.open(path).convert('RGB')
-        return self.transform(img)
+        # img = Image.open(path).convert('RGB')
+        # return self.transform(img)
+
+        cv_read_flag = cv2.IMREAD_COLOR
+        interpolation = cv2.INTER_LINEAR
+        cv_img_origin = cv2.imread(path, cv_read_flag)
+        new_width = 340
+        new_height = 256
+        cv_img = cv2.resize(cv_img_origin, (new_width, new_height), interpolation)
+        cv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+
+        return self.transform(cv_img)
 
     def __getitem__(self, index):
         frames_path, frames_ids, target = self.data[index]
+        print("frames_path: ", frames_path)
+        print("frames_ids: ", frames_ids)
         frames_zp = []
         frames_ze = []
         for i in range(len(frames_ids)):
             frame_path = os.path.join(frames_path, 'frame{}.jpg'.format(str(frames_ids[i]).zfill(6)))
+            print("frames_path", frame_path)
+            sys.stdout.flush()
             if i < self.zp_limit: 
                 img = self.get_transformed_frame(frame_path)
                 frames_zp.append(img)

model.py
@@ -1,5 +1,6 @@
 import math
 from torch import nn
+import torch.nn.functional as F
 from torchvision import models
 import torch
 import numpy as np
@@ -8,35 +9,36 @@ import time
 
 from efficientnet_pytorch import EfficientNet
 #from models import rgb_resnet152
-# from network import resnet101
+from network import resnet101
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # device = torch.device('cpu')
 
 class FrameFeats(nn.Module):
-    def __init__(self, out_dim):
+    def __init__(self, out_dim, backbone='efficientnetb0'):
         super().__init__()
         
         # self.pretrained_backbone = rgb_resnet152(pretrained=False, num_classes=101)
         # self.pretrained_backbone = models.resnet152(pretrained=False, num_classes=101)
         # self.pretrained_backbone.fc_action = self.pretrained_backbone.fc
         # pretrained_params_ucf101 = torch.load('checkpoints/ucf101_s1_rgb_resnet152.pth.tar')
-        
-        # self.pretrained_backbone = resnet101(pretrained=False, channel=3)
-        # pretrained_params_ucf101 = torch.load('ucf101_resnet101.pth.tar', map_location=device)
-        # num_ftrs = self.pretrained_backbone.fc_custom.in_features
-        # self.pretrained_backbone.fc_custom = nn.Linear(num_ftrs, out_dim)
 
-        self.pretrained_backbone = EfficientNet.from_name('efficientnet-b0')
-        self.pretrained_backbone._fc = nn.Linear(self.pretrained_backbone._bn1.num_features, 101)
-        pretrained_params_ucf101 = torch.load(
-            'ucf101_efficientnetb0.pth.tar',
-            map_location=device,
-        )
-        self.pretrained_backbone.load_state_dict(pretrained_params_ucf101['state_dict'])
-        num_ftrs = self.pretrained_backbone._fc.in_features
-        self.pretrained_backbone._fc = nn.Linear(num_ftrs, out_dim)
+        if backbone == 'efficientnetb0':
+            self.pretrained_backbone = EfficientNet.from_name('efficientnet-b0')
+            self.pretrained_backbone._fc = nn.Linear(self.pretrained_backbone._bn1.num_features, 101)
+            pretrained_params_ucf101 = torch.load(
+                'ucf101_efficientnetb0.pth.tar',
+                map_location=device,
+            )
+            self.pretrained_backbone.load_state_dict(pretrained_params_ucf101['state_dict'])
+            num_ftrs = self.pretrained_backbone._fc.in_features
+            self.pretrained_backbone._fc = nn.Linear(num_ftrs, out_dim)
+        elif backbone == 'resnet101':
+            self.pretrained_backbone = resnet101(pretrained=False, channel=3)
+            pretrained_params_ucf101 = torch.load('ucf101_resnet101.pth.tar', map_location=device)
+            num_ftrs = self.pretrained_backbone.fc_custom.in_features
+            self.pretrained_backbone.fc_custom = nn.Linear(num_ftrs, out_dim)
 
         # print('efficientnetb0 params:', sum([np.prod(p.size()) for p in filter(lambda p: p.requires_grad, self.pretrained_backbone.parameters())]))
         # print('resnet101 params:', sum([np.prod(p.size()) for p in filter(lambda p: p.requires_grad, resnet101(pretrained=False, channel=3).parameters())]))
@@ -49,7 +51,7 @@ class FrameFeats(nn.Module):
         return self.pretrained_backbone(frame)
 
 class TransformationNet(nn.Module):
-    def __init__(self, input_dim, dim, n_actions):
+    def __init__(self, input_dim, dim, n_actions, instance_norm=True, embed_dropout=.5, trans_dropout=0):
         super().__init__()
         self.dim = dim
         self.n_actions = n_actions
@@ -62,14 +64,26 @@ class TransformationNet(nn.Module):
         self.W_tranformations = nn.Parameter(torch.Tensor(n_actions, dim, dim))
         nn.init.kaiming_uniform_(self.W_tranformations, a=math.sqrt(5))
         self.default_action = torch.Tensor(list(range(self.n_actions)))
+        if instance_norm:
+            # self.instance_norm = nn.InstanceNorm1d(self.dim, affine=True)
+            self.instance_norm = nn.GroupNorm(1, 1)
+        else:
+            self.instance_norm = nn.Identity()
+        self.embed_dropout = nn.Dropout(p=embed_dropout, inplace=True)
+        self.trans_dropout = nn.Dropout(p=trans_dropout, inplace=True)
 
     def forward(self, precondition, effect, action=None):
         batch_size = precondition.shape[0]
         p_avg = precondition.sum(1).float() / (precondition != 0).sum(1).float()
         e_avg = effect.sum(1).float() / (effect != 0).sum(1).float()
 
+        p_avg = self.instance_norm(p_avg.unsqueeze(1)).squeeze(1)
+        e_avg = self.instance_norm(e_avg.unsqueeze(1)).squeeze(1)
+
         p_embed = self.precondition_proj(p_avg)
+        p_embed = self.embed_dropout(p_embed)
         e_embed = self.effect_proj(e_avg)
+        e_embed = self.embed_dropout(e_embed)
 
         # if action is None:
         #     action = self.default_action
@@ -88,6 +102,8 @@ class TransformationNet(nn.Module):
                 # e_embed_copy[:, i, :] = e_embed
             # e_embed = e_embed_copy
             e_embed = e_embed.unsqueeze(1).expand(batch_size, self.n_actions, self.dim).contiguous()
+
+        p_transformed = self.trans_dropout(p_transformed)
 
         # results = []
         # for action_idx in actions_idx:

train.py
@@ -18,7 +18,7 @@ import time
 frame_feats_dim = 512
 model_dim = 512
 n_actions = 101
-batch_size = 6
+batch_size = 4
 batch_size_to_step = 50
 iter_to_step = int(batch_size_to_step / batch_size)
 n_frames = 25
@@ -46,7 +46,7 @@ def train(epoch, log_path):
 
     train_log_path = os.path.join(log_path, 'train_log.txt')
     log_data = ''
-    net.train(True)
+    net.train()
     optimizer.zero_grad()
     loss_list = []
     acc_list = []
@@ -64,7 +64,7 @@ def train(epoch, log_path):
             p_transformed.view(output_loss_dim,  p_transformed_shape[2]).to(device),
             e_embed.view(output_loss_dim, p_transformed_shape[2]).to(device),
             y.view(output_loss_dim).to(device)
-        )
+        ) / iter_to_step
         # print("loss: ", loss.item())
         
         loss.backward()
@@ -88,7 +88,7 @@ def train(epoch, log_path):
         log_data += '{} - Epoch: {}; Loss: {:.5f};\n'.format(
             datetime.today().replace(microsecond=0),
             epoch + 1, loss.item(),
-            accuracy,
+            np.mean(acc_list),
         )
         sys.stdout.flush()
 
@@ -106,7 +106,7 @@ def valid(epoch, log_path):
     dataset = iter(valid_set)
     pbar = tqdm(dataset)
 
-    net.train(False)
+    net.eval()
     valid_log_path = os.path.join(log_path, 'val_log.txt')
     log_data = ''
     loss_list = []
@@ -151,7 +151,7 @@ def valid(epoch, log_path):
 
             log_data += '{} - Epoch: {}; Loss: {:.5f}; Acc: {:.5f}\n'.format(
                 datetime.today().replace(microsecond=0),
-                epoch + 1, loss.item(), accuracy
+                epoch + 1, loss.item(), np.mean(acc_list)
             )
     
     with open(valid_log_path, 'a+') as f: